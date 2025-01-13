18e9e8ec360ef2709ec1d2cc8d0ffb067a133363
MaksymSelezen <foqney@gmail.com>
2025-01-13 18:44:35 +0200

    fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+// const API_KEY = '755e93f2f2d052187e98e8094e446d4f';
 axios.defaults.baseURL = "https://api.themoviedb.org/3/";
 const ACCESS_TOKEN =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTcxNzZiMTRhNTdkMGIyNjgwYmFlZDA0Y2JmMjljZSIsIm5iZiI6MTczNjcxMzg0MS44NDA5OTk4LCJzdWIiOiI2Nzg0MjY3MTIyNTYwMjNkZmQ0ZTUzYzkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.lI4aUY2mMtZhd3P-Fih59JJdbE-y6_m5qcoOdHsE4f0";
